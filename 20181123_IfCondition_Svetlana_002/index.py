cd1a76f24e43c05536b2ca3e3cf122ac4973a876
Vadim <39588115+vnikifirov@users.noreply.github.com>
2018-11-23 14:52:46 +0300

    Add IfCondtion Svetlana

---
+++ 20181123_IfCondition_Svetlana_002/index.py
@@ -0,0 +1,10 @@
+def if_else_condition(num):
+    if (num == 1):
+        return 'Январь'
+    elif (num == 2):
+        return 'Февраль'
+    # If you wish, you can add more months    
+    else: 
+        return 'декабрь'
+
+if_else_condition(3)
